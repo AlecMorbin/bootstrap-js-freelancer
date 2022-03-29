985bba4f7e9e30ccb65c33b520f9a4fe56d2962c
Alec <alemor98@hotmail.it>
2022-03-29 14:32:13 +0200

    Inizio script

---
+++ main.js
@@ -0,0 +1,14 @@
+function submitForm(event){
+    event.preventDefault();
+    console.log("Clicked");
+}
+
+function calcolo(){
+    //backend 1 ora =  20.5 EUR
+    //frontend 1 ora = 15.3 EUR
+    //analisi 1 ora  = 33.6 EUR
+    
+    let listaCodiciSconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+
+}
