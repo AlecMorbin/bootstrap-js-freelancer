97ae24f664bdfd3667407fdc54a852eadc54498b
Alec <alemor98@hotmail.it>
2022-03-29 14:42:06 +0200

    testing per lo script

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function submitForm(event){
     event.preventDefault();
     console.log("Clicked");
+
+    document.getElementById("result").innerHTML = `Il prezzo finale è di: ${calcolo()}`;
 }
 
 function calcolo(){
@@ -10,5 +12,9 @@ function calcolo(){
     
     let listaCodiciSconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
-
+    let ore = document.getElementById("hours").value;
+    let lavoro = document.getElementById("work").value;
+    let result = 0;
+    console.log(typeof ore);
+    return ;
 }
