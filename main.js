7f5a86d8de2870707d7e249acb7b02ae4c359a4b
Alec <alemor98@hotmail.it>
2022-03-29 15:14:21 +0200

    Script base ultimato

--- main.js
+++ main.js
@@ -12,9 +12,37 @@ function calcolo(){
     
     let listaCodiciSconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
-    let ore = document.getElementById("hours").value;
+    let ore =  document.getElementById("hours").value;
     let lavoro = document.getElementById("work").value;
     let result = 0;
-    console.log(typeof ore);
-    return ;
+    switch (lavoro) {
+        case "backend":
+            result = ore * 20.5;
+            break;
+        case "frontend":
+            result = ore * 15.3;
+            break;       
+        case "analysis":
+            result = ore * 33.6;
+            break;
+    }
+
+    let discount  = document.getElementById("discount").value;
+
+    let flag = false;
+    for (let i = 0; i < listaCodiciSconto.length; i++) {   
+        if (discount == listaCodiciSconto[i]) {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag == true) {
+        result = (result*75)/100;
+    }
+    else {
+        alert("Il codice sconto non è valido");
+    }
+
+    return result.toFixed(2);
 }
